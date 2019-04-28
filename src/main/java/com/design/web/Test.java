e7e2db090b069162ea0f915d87faabddd5b70b70
fxx <fanxx_3516@163.com>
2019-04-28 09:52:07 +0800

    test commit

---
+++ src/main/java/com/design/web/Test.java
@@ -0,0 +1,12 @@
+package com.design.web;
+
+/**
+ * @ProjectName: pattern
+ * @Package: com.design.web
+ * @Description:
+ * @Author: fanxx
+ * @CreateDate: 2019/4/28 9:51
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+public class Test {
+}
