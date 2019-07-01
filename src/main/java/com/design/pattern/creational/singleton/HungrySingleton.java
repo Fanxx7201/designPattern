49a68106630d1acf2f308aeb7e4f2607ec370558
fxx <fanxx_3516@163.com>
2019-07-01 16:55:21 +0800

    singleton update

--- src/main/java/com/design/pattern/creational/singleton/HungrySingleton.java
+++ src/main/java/com/design/pattern/creational/singleton/HungrySingleton.java
@@ -16,7 +16,10 @@ public class HungrySingleton implements Serializable {
     private final static HungrySingleton hungrySingleton = new HungrySingleton();
 
     private HungrySingleton(){
-
+        //对于类加载的时候, 实例就已经创建好了这样的类时有效的.
+        if(hungrySingleton != null){
+            System.out.println("单例构造器禁止反射");
+        }
     }
 
     public static HungrySingleton getInstance(){
---
+++ src/main/java/com/design/pattern/creational/singleton/Test2.java
@@ -0,0 +1,34 @@
+package com.design.pattern.creational.singleton;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ProjectName: pattern
+ * @Package: com.design.pattern.creational.singleton
+ * @Description:
+ * @Author: fanxx
+ * @CreateDate: 2019/6/27 16:38
+ * <p>Copyright: Copyright (c) 2019</p>
+ */
+public class Test2 {
+    public static void main(String[] args) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
+        Class objectClass = HungrySingleton.class;
+        Constructor constructor = objectClass.getDeclaredConstructor();
+        //设置权限, 为true的时候, HungrySingleton的构造器private就会放开 -- 我们在HungrySingleton中需要写反射防御的代码
+        constructor.setAccessible(true);
+        // 单例拿出来一个对象
+        HungrySingleton instance = HungrySingleton.getInstance();
+
+        HungrySingleton newInstance = (HungrySingleton) constructor.newInstance();
+        System.out.println(instance);
+        System.out.println(newInstance);
+        System.out.println(instance == newInstance);
+
+
+        List<String> list = new ArrayList<>();
+
+    }
+}
